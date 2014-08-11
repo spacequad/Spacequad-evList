ea20b74d45363af20ccaa0feb66fdbf1ff86cdf5
Michael Brusletten <glfusion@spacequad.com>
2014-08-10 23:45:43 -0400

    Merge remote-tracking branch 'upstream/master'
    Conflicts:
    	css/style.css
    	docs/evlist.html
    	language/dutch_utf-8.php
    	public_html/js/calselect.js
    	templates/addeventoption.thtml
    	templates/centerblock.thtml
    	templates/dayview/column.thtml
    	templates/dayview/dayview.thtml
    	templates/dayview/dayview_print.thtml
    	templates/dayview/singleevent.thtml
    	templates/monthview/monthview.thtml
    	templates/monthview/monthview_print.thtml
    	templates/weekview/events.thtml
    	templates/weekview/weekview.thtml
    	templates/weekview/weekview_print.thtml

--- public_html/js/calselect.js
+++ public_html/js/calselect.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
 *   Calendar selection javascript.
 *   Allows the user to select or deselect calendars and have the 
@@ -63,3 +64,70 @@ function getCals(cls) {
     var el = getElementsByClass(document,cls,'*');
 }
 
+=======
+/**
+*   Calendar selection javascript.
+*   Allows the user to select or deselect calendars and have the 
+*   corresponding events displayed or removed from the calendar view
+*
+*   @author     Lee P. Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2011 Lee Garner <lee@leegarner.com>
+*   @package    evlist
+*   @version    1.3.0
+*   @license    http://opensource.org/licenses/gpl-2.0.php
+*               GNU Public License v2 or later
+*   @filesource
+*/
+
+function clickCalendarButton() {
+    var action = document.getElementById('calendar_dialog');
+    action.style.display="block";
+    action.style.visibility="visible";
+    return;
+}
+
+function closedialog() {
+    var action = document.getElementById('calendar_dialog');
+    action.style.display="none";
+    action.style.visibility="hidden";
+    return;
+}
+
+function SelectCal(cal_chk)
+{
+    var cls = "show_" + cal_chk.id;
+
+    var el = getElementsByClass(document,cls,'*');
+    if (cal_chk.checked == 1) {
+        //var newvis = 'visible';
+        var newvis = 'block';
+    } else {
+        //var newvis = 'hidden';
+        var newvis = 'none';
+    }
+    for (i = 0; i < el.length; i++) {
+        //el[i].style.visibility = newvis;
+        el[i].style.display = newvis;
+    }
+}
+
+function getElementsByClass(node,searchClass,tag)
+{
+    var classElements = new Array();
+    var els = node.getElementsByTagName(tag); // use "*" for all elements
+    var elsLen = els.length;
+    var pattern = new RegExp("\\b"+searchClass+"\\b");
+    for (i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+            classElements[j] = els[i];
+            j++;
+        }
+    }
+    return classElements;
+}
+
+function getCals(cls) {
+    var el = getElementsByClass(document,cls,'*');
+}
+
+>>>>>>> upstream/master
